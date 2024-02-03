2ecb2fe83b501754bf51acbb8b4fcdc4c59209f8
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-02-03 22:23:30 +0100

    Fix PSI

--- .eleventy.js
+++ .eleventy.js
@@ -81,8 +81,8 @@ module.exports = function(eleventyConfig) {
  
           return `<div class="image-wrapper blur-load" >
                   <svg 
-                    width="${largestSrc.width}" 
-                    height="${largestSrc.height}" 
+                    width="100%" 
+                    height="auto"
                     viewBox="0 0 ${largestSrc.width} ${largestSrc.height}" 
                     fill="none" 
                     xmlns="http://www.w3.org/2000/svg">
